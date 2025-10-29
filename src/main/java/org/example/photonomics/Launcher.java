2bf3c1dfc8fd1590a0821047d001c3f9d95aebbd
DuckOnQuack9 <will.turtlez@gmail.com>
2025-10-29 11:20:34 -0400

    added print for example

--- src/main/java/org/example/photonomics/Launcher.java
+++ src/main/java/org/example/photonomics/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.out.println("Launching JavaFX Application...");
         Application.launch(HelloApplication.class, args);
     }
 }
