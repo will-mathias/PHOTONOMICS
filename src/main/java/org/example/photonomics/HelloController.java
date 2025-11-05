12bfb54594cb537ec599bb03c2826f3775210d0b
DuckOnQuack9 <will.turtlez@gmail.com>
2025-11-05 11:02:54 -0500

    Fixed areas to fields

--- src/main/java/org/example/photonomics/HelloController.java
+++ src/main/java/org/example/photonomics/HelloController.java
@@ -1,8 +1,16 @@
 package org.example.photonomics;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    @FXML
+    TextField energyUsageField;
+    @FXML
+    TextField HouseholdIncomeField;
+    @FXML
+    ChoiceBox<String> regionDropDown;
 
     @FXML
     protected void onCalculateButtonClick() {
