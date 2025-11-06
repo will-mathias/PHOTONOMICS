b018335d52f9c24f799b650a26b8ad554c24bae0
DuckOnQuack9 <will.turtlez@gmail.com>
2025-11-06 13:31:08 -0500

    Populated dropdown

--- src/main/java/org/example/photonomics/HelloController.java
+++ src/main/java/org/example/photonomics/HelloController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+
 public class HelloController {
     @FXML
     TextField energyUsageField;
@@ -11,6 +13,36 @@ public class HelloController {
     TextField HouseholdIncomeField;
     @FXML
     ChoiceBox<String> regionDropDown;
+
+    @FXML
+    void initialize() {
+        initDropdown();
+
+    }
+
+    private void initDropdown() {
+        String[] regions = {
+                "Bomi",
+                "Bong",
+                "Gbarpolu",
+                "Grand Bassa",
+                "Grand Cape Mount",
+                "Grand Gedeh",
+                "Grand Kru",
+                "Lofa",
+                "Margibi",
+                "Maryland",
+                "Montserrado",
+                "Nimba",
+                "Rivercess",
+                "River Gee",
+                "Sinoe"
+        };
+
+        for (String region : regions) {
+            regionDropDown.getItems().add(region);
+        }
+    }
 
     @FXML
     protected void onCalculateButtonClick() {
