dcb587b0537154d580a78d606ec715245a13522a
Harsh Patel <harshnpatel2021@gmail.com>
2026-02-19 13:20:05 -0500

    removed hello world print statement

--- src/main/java/org/example/photonomics/Launcher.java
+++ src/main/java/org/example/photonomics/Launcher.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.println("hello world");
         Application.launch(CalculatorApp.class, args);
     }
 }
