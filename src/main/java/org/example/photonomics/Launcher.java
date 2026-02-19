7a0240ac874f1bb78b1b937d97171f129caa6ac0
Harsh Patel <harshnpatel2021@gmail.com>
2026-02-19 12:39:21 -0500

    hello world commit test

--- src/main/java/org/example/photonomics/Launcher.java
+++ src/main/java/org/example/photonomics/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        System.out.println("hello world");
         Application.launch(CalculatorApp.class, args);
     }
 }
